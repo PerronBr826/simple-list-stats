4e0d12156f9a3f927ba5ce5462824926d0965608
PerronBr826 <PerronBr826@northwested.org>
2024-10-28 10:30:54 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+
+# Chemisrty Scones
+
+chemistry_scores = [100, 89, 22, 10, 100, 15]
+
+scores_avg = sum(chemistry_scores)/len(chemistry_scores)
+print(f"The average of your chemistry scores is {scores_avg}!")
+
+# Driving Distances
+
+distances = [364, 4568, 2736, 1270, 632]
+
+shortest_dist = min(distances)
+longest_dist = max(distances)
+dist_avg = sum(distances)/len(distances)
+
+print(f'The shortest round trip between Traverse City and a different location is {shortest_dist}.')
+print(f'The longest round trip between Traverse City and a different location is {longest_dist}.')
+print(f'The average round trip on the list of distances between places and Traverse City is {dist_avg}.')
